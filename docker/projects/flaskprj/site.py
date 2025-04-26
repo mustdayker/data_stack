5034902fded9590ba1bdfa1e3e2c8bb9115db024
Dmitry Kosarev <mustdayker@gmail.com>
2025-04-26 14:07:52 +0300

    docker course

--- docker/projects/flaskprj/site.py
+++ docker/projects/flaskprj/site.py
@@ -1,12 +1,27 @@
 #site.py
 
 from flask import Flask
+import psycopg2
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello from Docker!</h1>'
+    
+    # Подобная форма установки соединения в реальной практике
+    # СТРОГО ЗАПРЕЩЕНА
+    # Данный пример сделан для удобства в учебных целях
+    
+    conn = psycopg2.connect(dbname='mydata', user='postgres',
+                            password='1234', host='dbps')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM women LIMIT 10')
+    records = cursor.fetchall()
+    
+    cursor.close()
+    conn.close()
+    
+    return '<h1>Hello from FlaskPrj!</h1>' + f'{"<p>".join(map(str, records))}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4000)
